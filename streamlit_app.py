6684bbb6278f577f033983a9e08647ead1dbff20
Ehtesham Fassih <110147434+eFassih@users.noreply.github.com>
2022-07-28 15:08:13 +1000

    Update streamlit_app.py
    added new build your own item

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avocade Toast')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
